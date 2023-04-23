79eb57f5c65d9a4387b2d665c3c6f89e3dfe38e4
Darya Evdokimova <evddaryaevg@gmail.com>
2023-04-23 20:11:04 +0700

    added global vars

--- 04_lab/task1/1a.c
+++ 04_lab/task1/1a.c
@@ -1,40 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-void function() {
+int globalNotInitedInt;
+int globalInitedInt = 5;
+char *globalArrayChar = "global array, privet!";
+
+int globalConstInt = 123;
+int globalConstNotInit;
+
+void showLocalAddresses() {
   // i
   int localVarInit = 5;
-  printf("local initialized: \t 0x%p (%d)\n", &localVarInit, localVarInit);
+  printf("local initialized: \t %p (%d)\n", &localVarInit, localVarInit);
 
   int localVarNotInit;
-  printf("local not init: \t 0x%p (%d)\n", &localVarNotInit, localVarNotInit);
+  printf("local not init: \t %p (%d)\n", &localVarNotInit, localVarNotInit);
 
   char *localCharArray = "Hello from local";
-  printf("local char arr: \t 0x%p (%s)\n", &localCharArray, localCharArray);
+  printf("local char arr: \t %p (%s)\n", &localCharArray, localCharArray);
 
   // ii
   static int staticInit = 1;
-  printf("static int init: \t 0x%p (%d)\n", &staticInit, staticInit);
+  printf("static int init: \t %p (%d)\n", &staticInit, staticInit);
 
   static int staticNotInit;
-  printf("static in not init: \t 0x%p (%d)\n", &staticNotInit, staticNotInit);
+  printf("static in not init: \t %p (%d)\n", &staticNotInit, staticNotInit);
 
   static char *staticChar = "Hello from static char";
-  printf("static char arr: \t 0x%p (%s)\n", &staticChar, staticChar);
+  printf("static char arr: \t %p 0x%p (%s)\n", &staticChar, staticChar, staticChar);
 
   // iii
   const int myConst = 78;
-  printf("const int: \t\t 0x%p (%d)\n", &myConst, myConst);
+  printf("const int: \t\t %p (%d)\n", &myConst, myConst);
 
   const char *constChar = "h e l l o   m y  c o n s t n";
-  printf("const char arr: \t 0x%p (%s)\n", &constChar, constChar);
+  printf("const char arr: \t %p (%s)\n", &constChar, constChar);
   
 }
 
 int main(void) {
 
-  printf("function():: \t\t 0x%p\n", function);
-  function();
+  printf("function(): \t\t %p\n", showLocalAddresses);
+  printf("========================================================\n"); 
+ 
+  printf("LOCAL VARIABLES\n");
+  showLocalAddresses();
+
+  printf("========================================================\n");
+
+  printf("GLOBAL VARIABLES\n");
+  printf("global not inited int: \t\t %p (%d)\n", &globalNotInitedInt, globalNotInitedInt);
+  printf("global inited int: \t\t %p (%d)\n", &globalInitedInt, globalInitedInt);
+  printf("global array char: \t\t %p 0x%p (%s)\n", &globalArrayChar, globalArrayChar, globalArrayChar);
+
+  printf("global const int inited: \t %p (%d)\n", &globalConstInt, globalConstInt);
+  printf("global const int not inited: \t %p (%d)\n", &globalConstNotInit, globalConstNotInit);
+
+  printf("========================================================\n");
+ 
+  printf("pid: %d\n", getpid());
+
+  sleep(30);
 
   return 0;
 }
