532241327470134069314f1525caadf6b5dc3b5f
Darya Evdokimova <evddaryaevg@gmail.com>
2023-05-19 18:41:36 +0700

    start pages

--- 04_lab/task2/ab.c
+++ 04_lab/task2/ab.c
@@ -12,7 +12,7 @@ int main(int argc, char **argv) {
   sleep(1);
 
   // iii
-  if (execv("./exec", argv) == -1) {
+  if (execv("argv[0]", argv) == -1) {
    perror("execv()");
   }
 
@@ -21,3 +21,11 @@ int main(int argc, char **argv) {
 
   return 0;
 }
+
+
+/*
+exec не меняет меняет процесс, меняется только АП
+
+Каждый раз когда делается exec ОС берет старое пространство 
+и выкидывает его и создает новое. 
+*/
---
+++ 04_lab/task2/cde_page.c
@@ -0,0 +1,41 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+int main() {
+  printf("PID: %d\n", getpid());
+  sleep(10);
+
+  void *p = mmap(NULL, 10 * getpagesize(),
+                 PROT_READ | PROT_WRITE | PROT_EXEC,
+                 MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+  printf("Mapped memory: p = %p\n", p);
+  sleep(20);
+
+ 
+  // mprotect(p, 10 * getpagesize(), PROT_READ);
+
+  mprotect(p, 10 * getpagesize(), PROT_WRITE);
+
+  char *array = (char *)p;
+  // char symbol = array[1];
+
+  // printf("%c\n", symbol); // we've read from mapped region
+
+  // array[10] = 'a'; // here we got seg fault, cause we have no
+                   // permission for writing in this region
+
+  ////////////////////////////////
+
+  for(int i = 0; i < 15; i++) {
+    array[i] = i + 1;
+    printf("%c ", array[i]);
+  }
+
+  // printf("%c\n", symbol);
+  // array[10] = 'a';
+
+  return 0;
+}
